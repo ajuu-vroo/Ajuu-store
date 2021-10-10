e5ab9881d0686a831cbc5749138ffebbca8aac28
TheTruUchiha <75875192+TheTruUchiha@users.noreply.github.com>
2021-10-10 17:10:05 +0530

    added Head tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@ import 'tailwindcss/tailwind.css'
 import Navbar from '../components/Navbar'
 import { Provider } from 'next-auth/client'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }) {
   return (
   <Provider session={pageProps.session}>
+  <Head>
+  <meta http-equiv="Content-Security-Policy" content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *;**script-src 'self' http://onlineerp.solution.quebec 'unsafe-inline' 'unsafe-eval';** ">
+  </Head>
   <div className='font-nunito'>
   <Navbar />
   <Component {...pageProps} />
